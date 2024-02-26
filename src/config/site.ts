6d0768ab1b93a177e95d37448d61bc34d17e14b8
Spencer Craigie <sscraigie@live.com>
2024-02-25 21:26:17 -0800

    Added config files

---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "Spencer Craigie",
+  url: "https://www.spencercraigie.com",
+  ogImage: "",
+  description: "My developer portfolio",
+  links: {
+    twitter: "https://twitter.com/sscraigie",
+    github: "https://github.com/sscraigie",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
