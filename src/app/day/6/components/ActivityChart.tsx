404250a09cd5ecaf7855e4f702938f437554037d
Spencer Craigie <sscraigie@live.com>
2024-02-26 22:26:57 -0800

    removed daily-ui days

--- src/app/day/6/components/ActivityChart.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const colors = ["#0e4429", "#006d32", "#26a641", "#39d353"];
-
-export const ActivityChart = () => {
-  return (
-    <div className="flex w-1/2 flex-col gap-5 rounded-lg border border-solid border-[#31363d] p-3 "></div>
-  );
-};
