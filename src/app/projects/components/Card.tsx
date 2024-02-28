7e2c9d36d815715289ffb9c38226d762d28d3f14
Spencer Craigie <sscraigie@live.com>
2024-02-28 00:23:22 -0800

    Fixed project cards

--- src/app/projects/components/Card.tsx
+++ src/app/projects/components/Card.tsx
@@ -1,32 +1,29 @@
 "use client";
 import React from "react";
 import { PinContainer } from "@/components/ui/3d-pin";
+import { project } from "../page";
 
-export function Card(props: {
-  title: string;
-  description: string;
-  href: string;
-  image?: string;
-}) {
+export default function Card({ project }: { project: project }) {
+  const { title, description, href, image } = project;
   return (
-    <div className="flex h-[40rem] w-full items-center justify-center ">
-      <PinContainer title={props.href} href={`https://${props.href}`}>
-        <div className="flex h-[20rem] w-[20rem] basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 ">
-          <h3 className="!m-0 max-w-xs !pb-2 text-base  font-bold text-slate-100">
-            {props.title}
-          </h3>
-          <div className="!m-0 !p-0 text-base font-normal">
-            <span className="text-slate-500 ">{props.description}</span>
-          </div>
-          <div className="mt-4 flex w-full flex-1 rounded-lg bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500">
+    <PinContainer title={href} href={`https://${href}`}>
+      <div className="flex h-[20rem] w-[20rem] basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 ">
+        <h3 className="!m-0 max-w-xs !pb-2 text-base  font-bold text-slate-100">
+          {title}
+        </h3>
+        <div className="!m-0 !p-0 text-base font-normal">
+          <span className="text-slate-500 ">{description}</span>
+        </div>
+        <div className="mt-4 flex w-full flex-1 rounded-lg bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500">
+          {image && (
             <img
-              src={props.image}
-              alt={props.title}
+              src={image}
+              alt={title}
               className="h-full w-full rounded-lg object-cover"
             />
-          </div>
+          )}
         </div>
-      </PinContainer>
-    </div>
+      </div>
+    </PinContainer>
   );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,30 +1,51 @@
 import React from "react";
-import { InfoCard } from "../day/45/page";
+import Card from "./components/Card";
 import { HeroParallaxSection } from "./components/HeroParallax";
 
-const DAYS = [
-  { day: 1, title: "Sign Up" },
-  { day: 2, title: "Checkout" },
-  { day: 3, title: "Landing Page" },
-  { day: 4, title: "Calculator" },
-  { day: 5, title: "App Icon" },
-  { day: 6, title: "User Profile" },
-  { day: 8, title: "404 Page" },
-  { day: 11, title: "Flash Message" },
-  { day: 15, title: "Toggle" },
-  { day: 45, title: "InfoCard" },
-];
+export type project = {
+  title: string;
+  description: string;
+  href: string;
+  image?: string;
+};
 
+const PROJECTS: project[] = [
+  {
+    title: "Daily Dev",
+    description: "100 days developer challenge",
+    href: "dailyui.spencercraigie.com",
+    image: "/projects/daily-dev.png",
+  },
+  {
+    title: "Harvard Classics",
+    description: "Reading the Harvard Classics in 365 days",
+    href: "harvardclassics.spencercraigie.com",
+    image: "/projects/harvard.png",
+  },
+  {
+    title: "Speed Square",
+    description: "A 2D rubik's cube game",
+    href: "speedsquare.spencercraigie.com",
+    image: "/projects/speedSquare.png",
+  },
+  {
+    title: "Ask Spencer",
+    description:
+      "A ChatGPT clone that was built in an afternoon and a locally running LLM model on my home server",
+    href: "ask.spencercraigie.com",
+  },
+];
 const Portfolio = () => {
   return (
     <div className="w-full">
       <HeroParallaxSection />
-      <div className=" flex h-full w-full flex-col justify-center">
-        <div className="m-0 w-full max-w-5xl ">
-          <h1 className="m-0 p-10">Portfolio</h1>
-          <div className="flex max-w-4xl flex-wrap justify-center gap-5">
-            {DAYS.map((day) => (
-              <InfoCard key={day.day} day={day.day} title={day.title} />
+      <div className="flex w-full justify-center">
+        <div className="flex max-w-7xl flex-col ">
+          <h1 className="p-10 text-3xl">Portfolio</h1>
+          <div className="flex w-full flex-wrap items-center justify-center ">
+            {PROJECTS.map((project) => (
+              <Card project={project} />
+              // <InfoCard key={day.day} day={day.day} title={day.title} />
             ))}
           </div>
         </div>
