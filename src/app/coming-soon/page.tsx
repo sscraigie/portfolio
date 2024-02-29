aa46deb7bdb28703406dbddcbe735b42aa1da520
Spencer Craigie <sscraigie@live.com>
2024-02-29 13:06:54 -0800

    Created a coming soon page

---
+++ src/app/coming-soon/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+import { Spotlight } from "./Spotlight";
+
+export default function SpotlightPreview() {
+  return (
+    <div className="bg-grid-white/[0.02] bg-blue[0.96] relative flex h-full w-full overflow-hidden rounded-md antialiased md:items-center md:justify-center">
+      <Spotlight
+        className="-top-40 left-0 md:-top-20 md:left-60"
+        fill="white"
+      />
+      <div className=" relative z-10  mx-auto w-full max-w-7xl  p-4 pt-20 md:pt-0">
+        <h1 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-7xl">
+          {/* Spotlight <br /> is the new trend. */}
+          Page Coming <br />
+          Soon
+        </h1>
+        <p className="mx-auto mt-4 max-w-lg text-center text-base font-normal text-neutral-300"></p>
+      </div>
+    </div>
+  );
+}
