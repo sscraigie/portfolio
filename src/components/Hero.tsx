dab8d6992cca5da22da7c8dd9255993815571107
Spencer Craigie <sscraigie@live.com>
2024-02-29 13:12:16 -0800

    fixed mobild view a bit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,9 +4,9 @@ import Spline from "@splinetool/react-spline";
 
 export const Hero = () => {
   return (
-    <div className="flex w-full items-center justify-around py-10">
-      <div className="py-19 top-0 mx-auto  w-full max-w-7xl px-20 md:py-40">
-        <h1 className="text-2xl font-bold dark:text-white md:text-7xl">
+    <div className="flex w-full flex-col items-center justify-around py-10 md:flex-row">
+      <div className="py-19 top-0 mx-auto w-full max-w-7xl px-20 md:py-40">
+        <h1 className="text-4xl font-bold dark:text-white md:text-7xl">
           Hello 👋 <br /> I&apos;m Spencer
         </h1>
         <p className="mt-8 max-w-2xl text-base dark:text-neutral-200 md:text-xl">
