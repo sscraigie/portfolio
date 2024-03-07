f09138fd95ba16df8574db7b0d00c0bc586b7856
Spencer Craigie <sscraigie@live.com>
2024-03-06 23:28:16 -0800

    TechScroll component wip

---
+++ src/components/TechScroll.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function TechScoll() {
+  return (
+    <div className="inline-flex w-full flex-nowrap">
+      <ul className="flex items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-9">
+        <li>
+          <img src="./facebook.svg" alt="Facebook" />
+        </li>
+        <li>
+          <img src="./disney.svg" alt="Disney" />
+        </li>
+        <li>
+          <img src="./airbnb.svg" alt="Airbnb" />
+        </li>
+        <li>
+          <img src="./apple.svg" alt="Apple" />
+        </li>
+        <li>
+          <img src="./spark.svg" alt="Spark" />
+        </li>
+        <li>
+          <img src="./samsung.svg" alt="Samsung" />
+        </li>
+        <li>
+          <img src="./quora.svg" alt="Quora" />
+        </li>
+        <li>
+          <img src="./sass.svg" alt="Sass" />
+        </li>
+      </ul>
+    </div>
+  );
+  return <div>TechScoll</div>;
+}
