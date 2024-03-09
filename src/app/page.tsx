18cf0c3f1178989993d298d16598f5a4aacfcb4d
Spencer Craigie <sscraigie@live.com>
2024-03-08 21:08:07 -0800

    Tech scroll bar wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Hero } from "@/components/Hero";
+import TechScoll from "@/components/TechScroll";
 
 export default function HeroPage() {
   return (
     <div>
       <Hero />
+      <TechScoll />
     </div>
   );
 }
