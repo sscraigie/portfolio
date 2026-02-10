b08587f3f79f58c6357db50599a5a5a593863c28
Spencer Craigie <spencer.craigie@lightcast.io>
2026-02-10 06:49:05 -0800

    Fixed hero alignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Hero } from "@/components/Hero";
 
 export default function HeroPage() {
   return (
-    <div className="flex justify-center">
+    <div className="flex h-[75vh] items-align-center justify-center pt-8">
       <Hero />
     </div>
   );
