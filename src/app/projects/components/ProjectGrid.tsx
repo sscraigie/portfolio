c4654290a3d3511d08011882fa9ff7e8047f1cf1
Spencer Craigie <spencer.craigie@lightcast.io>
2026-02-10 20:47:50 -0800

    Fixed some cards

--- src/app/projects/components/ProjectGrid.tsx
+++ src/app/projects/components/ProjectGrid.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import React, { useEffect, useId, useRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useOutsideClick } from "@/hooks/use-outside-click";
-import { PinContainer  } from "@/components/ui/3d-pin";
+import { PinContainer } from "@/components/ui/3d-pin";
 
 export function ExpandableCard() {
   const [active, setActive] = useState<(typeof PROJECTS)[number] | boolean | null>(
@@ -39,13 +39,20 @@ export function ExpandableCard() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
+            transition={{ duration: 0.1 }}
             className="fixed inset-0 bg-black/20 h-full w-full z-10"
           />
         )}
       </AnimatePresence>
       <AnimatePresence>
         {active && typeof active === "object" ? (
-          <div className="fixed inset-0  grid place-items-center z-[100]">
+          <motion.div 
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            className="fixed inset-0 grid place-items-center z-[100]"
+          >
             <motion.button
               key={`button-${active.title}-${id}`}
               layout
@@ -53,11 +60,12 @@ export function ExpandableCard() {
                 opacity: 0,
               }}
               animate={{
+                opacity: 1,
               }}
               exit={{
                 opacity: 0,
                 transition: {
-                  duration: 0.05,
+                  duration: 0.15,
                 },
               }}
               className="flex absolute top-2 right-2 lg:hidden items-center justify-center bg-white rounded-full h-6 w-6"
@@ -68,7 +76,7 @@ export function ExpandableCard() {
             <motion.div
               layoutId={`card-${active.title}-${id}`}
               ref={ref}
-              className="w-full max-w-[500px]  h-full md:h-fit md:max-h-[90%]  flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
+              className="w-full max-w-[500px] h-full md:h-fit md:max-h-[90%] flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
             >
               <motion.div layoutId={`image-${active.title}-${id}`}>
                 <Image
@@ -104,7 +112,7 @@ export function ExpandableCard() {
                     exit={{ opacity: 0 }}
                     href={`https://{active.href}`}
                     target="_blank"
-                    className="px-4 py-3 text-sm rounded-full font-bold bg-green-500 text-white"
+                    className="px-4 py-3 text-sm rounded-md font-bold bg-white text-black"
                   >
                     Visit
                   </motion.a>
@@ -122,10 +130,10 @@ export function ExpandableCard() {
                 </div>
               </div>
             </motion.div>
-          </div>
+          </motion.div>
         ) : null}
       </AnimatePresence>
-       <div className="p-2 px-20 py-16 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-y-24 gap-4">
+      <div className="p-2 px-20 py-16 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-y-24 gap-4">
         <h1 className="lg:col-span-2 2xl:col-span-3 font-bold text-5xl">Projects</h1>
         {PROJECTS.map((card, index) => (
           <motion.div
@@ -134,39 +142,39 @@ export function ExpandableCard() {
             onClick={() => setActive(card)}
             className="p-4 flex flex-col rounded-xl cursor-pointer"
           >
-          <PinContainer className="w-96"  title={card.href} href={`https://${card.href}`}>
-            <div className="flex gap-4 flex-col  w-full">
-      <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 ">
-                <motion.h3
-                  layoutId={`title-${card.title}-${id}`}
-                  className="!m-0 max-w-xs !pb-2 text-base  font-bold text-slate-100"
-                >
-                  {card.title}
-                </motion.h3>
-                <motion.p
-                  layoutId={`description-${card.description}-${id}`}
-                  className="pb-2 text-slate-500 "
-                >
-                  {card.description}
-                </motion.p>
-              <motion.div className="w-full" layoutId={`image-${card.title}-${id}`}>
+            <PinContainer className="w-96" title={card.href} href={`https://${card.href}`}>
+              <div className="flex gap-4 flex-col  w-full">
+                <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 ">
+                  <motion.h3
+                    layoutId={`title-${card.title}-${id}`}
+                    className="!m-0 max-w-xs !pb-2 text-base  font-bold text-slate-100"
+                  >
+                    {card.title}
+                  </motion.h3>
+                  <motion.p
+                    layoutId={`description-${card.description}-${id}`}
+                    className="pb-2 text-slate-500 "
+                  >
+                    {card.description}
+                  </motion.p>
+                  <motion.div className="w-full" layoutId={`image-${card.title}-${id}`}>
 
-                {card.image && (
-                  <Image
-                    width={100}
-                    height={100}
-                    src={card.image}
-                    alt={card.title}
-                    className="w-full rounded-lg object-cover"
-                  />
-                )}
-              </motion.div>
+                    {card.image && (
+                      <Image
+                        width={100}
+                        height={100}
+                        src={card.image}
+                        alt={card.title}
+                        className="w-full rounded-lg object-cover"
+                      />
+                    )}
+                  </motion.div>
+                </div>
               </div>
-            </div>
-         </PinContainer>
+            </PinContainer>
           </motion.div>
         ))}
-        </div>
+      </div>
     </>
   );
 }
@@ -204,98 +212,6 @@ export const CloseIcon = () => {
   );
 };
 
-const cards = [
-  {
-    description: "Lana Del Rey",
-    title: "Summertime Sadness",
-    src: "https://assets.aceternity.com/demos/lana-del-rey.jpeg",
-    ctaText: "Visit",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        <p>
-          Lana Del Rey, an iconic American singer-songwriter, is celebrated for
-          her melancholic and cinematic music style. Born Elizabeth Woolridge
-          Grant in New York City, she has captivated audiences worldwide with
-          her haunting voice and introspective lyrics. <br /> <br /> Her songs
-          often explore themes of tragic romance, glamour, and melancholia,
-          drawing inspiration from both contemporary and vintage pop culture.
-          With a career that has seen numerous critically acclaimed albums, Lana
-          Del Rey has established herself as a unique and influential figure in
-          the music industry, earning a dedicated fan base and numerous
-          accolades.
-        </p>
-      );
-    },
-  },
-  {
-    description: "Babbu Maan",
-    title: "Mitran Di Chhatri",
-    src: "https://assets.aceternity.com/demos/babbu-maan.jpeg",
-    ctaText: "Visit",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        <p>
-          Babu Maan, a legendary Punjabi singer, is renowned for his soulful
-          voice and profound lyrics that resonate deeply with his audience. Born
-          in the village of Khant Maanpur in Punjab, India, he has become a
-          cultural icon in the Punjabi music industry. <br /> <br /> His songs
-          often reflect the struggles and triumphs of everyday life, capturing
-          the essence of Punjabi culture and traditions. With a career spanning
-          over two decades, Babu Maan has released numerous hit albums and
-          singles that have garnered him a massive fan following both in India
-          and abroad.
-        </p>
-      );
-    },
-  },
-
-  {
-    description: "Metallica",
-    title: "For Whom The Bell Tolls",
-    src: "https://assets.aceternity.com/demos/metallica.jpeg",
-    ctaText: "Visit",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        <p>
-          Metallica, an iconic American heavy metal band, is renowned for their
-          powerful sound and intense performances that resonate deeply with
-          their audience. Formed in Los Angeles, California, they have become a
-          cultural icon in the heavy metal music industry. <br /> <br /> Their
-          songs often reflect themes of aggression, social issues, and personal
-          struggles, capturing the essence of the heavy metal genre. With a
-          career spanning over four decades, Metallica has released numerous hit
-          albums and singles that have garnered them a massive fan following
-          both in the United States and abroad.
-        </p>
-      );
-    },
-  },
-  {
-    description: "Lord Himesh",
-    title: "Aap Ka Suroor",
-    src: "https://assets.aceternity.com/demos/aap-ka-suroor.jpeg",
-    ctaText: "Visit",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        <p>
-          Himesh Reshammiya, a renowned Indian music composer, singer, and
-          actor, is celebrated for his distinctive voice and innovative
-          compositions. Born in Mumbai, India, he has become a prominent figure
-          in the Bollywood music industry. <br /> <br /> His songs often feature
-          a blend of contemporary and traditional Indian music, capturing the
-          essence of modern Bollywood soundtracks. With a career spanning over
-          two decades, Himesh Reshammiya has released numerous hit albums and
-          singles that have garnered him a massive fan following both in India
-          and abroad.
-        </p>
-      );
-    },
-  },
-];
 type project = {
   title: string,
   description: string,
