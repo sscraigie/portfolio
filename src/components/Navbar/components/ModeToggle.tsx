83bdaa2c3966327185887f9d73262735e6a81ec2
Spencer Craigie <spencer.craigie@lightcast.io>
2026-07-18 09:13:35 -0700

    Fixed light mode icon

--- src/components/Navbar/components/ModeToggle.tsx
+++ src/components/Navbar/components/ModeToggle.tsx
@@ -21,7 +21,7 @@ export function ModeToggle() {
       className="w-9 px-0"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
-      <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <SunIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
